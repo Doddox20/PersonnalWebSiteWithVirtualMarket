27ee318ba294a699616035d9a9b14f0c51099039
Dorian Contal <144674543+Doddox20@users.noreply.github.com>
2023-09-19 16:22:16 +0200

    Update mysql_config.class.php

--- configs/mysql_config.class.php
+++ configs/mysql_config.class.php
@@ -1,10 +1,7 @@
 <?php
 
 class MysqlConfig {
-     //   const SERVEUR = '127.0.0.1' ;
-     //   const BASE = 'contal1';
-     //   const UTILISATEUR = 'contal';
-     //   const MOT_DE_PASSE = 'qwubb';
+    
        const SERVEUR = 'localhost' ;
         const BASE = 'ppe_marchand_contall';
         const UTILISATEUR = 'root';
